2a4d15b924136808ff3cc7f76c70973b9910b1d6
n.shaidulin <n.shaidulin@textback.io>
2021-08-22 19:30:41 +0300

    init

---
+++ src/main/java/com/shaidulin/kuskusbot/KusKusBotApplication.java
@@ -0,0 +1,13 @@
+package com.shaidulin.kuskusbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusKusBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KusKusBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shaidulin/kuskusbot/KusKusBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaidulin.kuskusbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusKusBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
